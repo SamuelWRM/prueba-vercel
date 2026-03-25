e8ede29292583674796501b97e17fd4c76f6e823
Samuel Rodriguez Contreras <contrerazwarmaster12@gmail.com>
2026-03-24 19:52:41 -0500

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,43 @@
+const controller = new AbortController();
+
+function TimeOutFunc() {
+  controller.abort();
+}
+
+//async sirve para cuando se utiloce el await
+async function GetMethod() {
+  const get_time = setTimeout(TimeOutFunc(), 5000);
+  var getbox = document.getElementById("getResponse");
+
+  try {
+    getbox.innerText = "Enviando peticion...";
+    const respuesta = await fetch("http://localhost:8000/estado");
+    const datos = await respuesta.json();
+
+    //el datos. "valor" hace referencia a la llave del diccionario
+    getbox.innerText = datos.valor;
+  } catch {
+    getbox.innerText = "Error al recuperar los datos";
+  }
+}
+
+async function PostMethod() {
+  const post_time = setTimeout(TimeOutFunc(), 5000);
+
+  var postbox = document.getElementById("postResponse");
+  var txtPostvalue = document.getElementById("postBody").value;
+
+  postbox.innerText = "Enviando datos...";
+  // sintaxis del post
+  try {
+    const respuesta = await fetch("http://localhost:8000/mod", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ num: txtPostvalue }),
+    });
+
+    postbox.innerText = "Datos enviados";
+  } catch {
+    postbox.innerText = "Error al enviar los datos";
+  }
+}
